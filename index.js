9febcdce221cec3f5989aee536cad0f13c1a5520
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-04 15:02:47 +0100

    add cli, generator and ejs modules

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
 
-console.log("soon");
+var path      = require("path"),
+    generator = require("./lib/generator"),
+    cli       = require("./lib/cli");
+
+cli.add('name', 'What is the name of your package ?', path.basename(path.resolve('.')))
+  .add('version', 'what is its version ?', '0.0.0')
+  .add('description', 'write a little description')
+  .add('keywords', 'some keywords seperated by commas', '', function (entry) { 
+    return entry.split(',').map(function (item) { 
+      return item.trim() 
+    }) 
+  })
+  .add('twitter', 'your Twitter username')
+  .add('github', 'your GitHub username')
+  .ask(function (data) {
+    generator(path.resolve('.'), data, function (err) {
+      if(err) console.log(err);
+      process.exit(0);
+    });
+  });
